3aba8647fb6f692be8179279db266dd3ba2b6558
Abdul Mustapha <abdulrmustapha@gmail.com>
2022-10-24 20:29:07 +0100

    Search API is connected
    Returns JSON Object based on user search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,29 +5,32 @@ import axios from "axios";
 
 const axiosConfig = {
   headers: {
-    Authorization: "Bearer ",
+    Authorization: "Bearer",
   },
-};
-
-const queryString = "q=" + encodeURIComponent("GitHub");
-
-const handleSearch = () => {
-  axios
-    .get(
-      "https://api.github.com/search/repositories?" + queryString,
-      axiosConfig
-    )
-    .then(function (response) {
-      console.log(response);
-    });
 };
 
 const Search = () => {
   const [userInput, setUserInput] = useState("");
-  
-  const handleChange = (event) => {
-    setUserInput(event.target.value);
+  const [queryData, setQueryData] = useState({});
+
+  const handleSearch = (event) => {
+    console.log("Search API Called");
+
+    let queryString = `q=${userInput}`;
+
+    axios
+      .get(
+        "https://api.github.com/search/repositories?" + queryString,
+        axiosConfig
+      )
+      .then(function (response) {
+        setQueryData(response.data.items);
+        
+      });
+	 
   };
+
+  console.log(queryData);
 
   return (
     <div className="flex items-center space-x-3">
@@ -35,12 +38,11 @@ const Search = () => {
         type="text"
         placeholder="Search all of Github..."
         className="pr-2 pl-6 py-2 rounded-full w-72"
-        onChange={handleChange}
-        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
       ></input>
       <button
         className="bg-white text-lg p-3 rounded-full"
-        onSubmit={handleSearch()}
+        onClick={(e) => handleSearch(e)}
       >
         <FiSearch className="" />
       </button>
